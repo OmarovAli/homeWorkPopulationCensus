82dc886246529e517f993ba6b12832b49a39b42f
Ali Omarov <omarovali425@gmail.com>
2023-02-03 22:14:30 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,79 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)]));
+
+            }
+        long count = persons.stream()
+                .filter(x -> x.getAge() < 18)
+                .count();
+        System.out.println(count + " несовершеннолетних");
+        List<String> stream = persons.stream()
+                .filter(x -> x.getAge() > 18)
+                .filter(x -> x.getAge() < 27)
+                .map(Person::toString)
+                .collect(Collectors.toList());
+        System.out.println( + " призывников");
+        Stream<Person> stream1 = persons.stream()
+                .filter(x -> x.getAge() > 18)
+                .filter(x -> x.getAge() < 65)
+    }
+}
+class Person {
+    private String name;
+    private String family;
+    private Integer age;
+    private Sex sex;
+    private Education education;
+
+    public Person(String name, String family, int age, Sex sex, Education education) {
+        this.name = name;
+        this.family = family;
+        this.age = age;
+        this.sex = sex;
+        this.education = education;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public Education getEducation() {
+        return education;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", family='" + family + '\'' +
+                ", age=" + age +
+                ", sex=" + sex +
+                ", education=" + education +
+                '}';
+    }
+}
